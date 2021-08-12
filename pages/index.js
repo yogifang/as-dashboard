6091ea87a21513b67bd777259d8bdc5803744a5f
yogifang <yogi@YogideMac-mini.local>
2021-08-12 16:04:21 +0800

    coding 0812-2021

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,45 @@
 import Link from 'next/link';
+
 import fetch from 'isomorphic-unfetch';
-import { Button, Card } from 'react-bootstrap';
+import { Button, Card  , Form} from 'react-bootstrap';
+import { useState } from 'react';
 
 const Index = ({members}) => {
+const [value , setValue] = useState('shooting') ;
+ 
+
+ const handleChange = (event) => {
+  setValue(event.target.value);
+    
+  }
+
   return(
     <div className="members-container">
-      <h1>Members</h1>
+      <h1>運動項目</h1>
       <div className="grid wrapper">
+
+      <Form.Select aria-label="Default select example" value={value} onChange={handleChange}>
+      <option value="shooting">射擊</option>
+      <option value="baseball">棒球</option>
+      </Form.Select>
+
+
+      <h1>選手</h1>
+      <Form.Select aria-label="Default select example">
+
       {members.map(note => {
+         
+         console.log(value) ;
+          if (note.sportItem !== value ) return null ;
+          console.log(note.sportItem) ;
           return (
-            <div key={note._id}>
-              <a>{note.email}</a>
-              
-            </div>
+              <option>
+              {note.email}
+            </option>
+           
           )
         })}
+        </Form.Select>
       </div>
     </div>
   ) ;

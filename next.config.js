941235ac051c3c7fec3f8f8838bc06b7e197d83d
yogifang <yogifang@gmail.com>
2021-09-12 20:06:38 +0800

    alpha0.61

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@ module.exports = {
   reactStrictMode: true,
   env : {
     MONGO_URI: "mongodb+srv://yogi:askYogi0325@cluster0.qgucu.mongodb.net/asia-scouting?retryWrites=true&w=majority" ,
-    HOST_URI: "http://localhost:3000/" 
-    //HOST_URI: "https://dashboard-chi-three.vercel.app/" 
+   // HOST_URI: "http://localhost:3000/" 
+    HOST_URI: "https://dashboard-chi-three.vercel.app/" 
   } ,
   async headers() {
     return [
